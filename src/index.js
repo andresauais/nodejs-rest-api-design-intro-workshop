15b018bcc946c2927e19e5fd2a6014c1235d5c6c
Andres Auais <andresauais@gmail.com>
2021-04-08 10:43:16 +0100

    algo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ connect().then(async function onServerInit() {
   config.logger.info(`DB connected`);
 
   // uncomment if you need to seed the database before
-  // await seedBooks();
+  //await seedBooks();
 
   app.listen(config.app.PORT, () => {
     config.logger.info(`Server running at http://localhost:${config.app.PORT}`);
